309f412156637ddd1860e516af452bdbf00d9ef7
burak2482 <burak2482@gmail.com>
2024-10-07 21:16:55 +0300

    İlk commit mesajı

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,6 +1,6 @@
 export default function Button({children}) {
   return (
-    <button class="bg-customGrey text-white font-medium text-sm hover:bg-customGreen py-3 px-12 rounded-md w-full">
+    <button class='bg-customGrey text-white font-medium text-sm hover:bg-customGreen py-3 px-12 rounded-md w-full'>
       {children}
     </button>
   );
--- app/page.js
+++ app/page.js
@@ -4,17 +4,17 @@ import './globals.css';
 
 export default function Home() {
   return (
-    <main class="flex justify-center items-center min-h-screen bg-customDarkBlack">
-      <div class="flex justify-center items-center flex-col bg-customBlack px-8 py-9 rounded-md">
+    <main class='flex justify-center items-center min-h-screen bg-customDarkBlack'>
+      <div class='flex justify-center items-center flex-col bg-customBlack px-8 py-9 rounded-md'>
         <div>
-          <Image class="rounded-full" src='/avatar-jessica.jpeg' width={100} height={100} />
+          <Image class='rounded-full' src='/avatar-jessica.jpeg' width={100} height={100} alt='Jessica Randall' />
         </div>
-        <div class="flex flex-col justify-center items-center mt-6">
-          <h1 class="text-white font-custom font-semibold text-xl">Jessica Randall</h1>
-          <h2 class="text-customGreen font-custom font-medium text-sm mt-1">London United Kingdom</h2>
-          <h2 class="font-custom mt-3 text-sm">"Front-end developer and avid reader."</h2>
+        <div class='flex flex-col justify-center items-center mt-6'>
+          <h1 class='text-white font-custom font-semibold text-xl'>Jessica Randall</h1>
+          <h2 class='text-customGreen font-custom font-medium text-sm mt-1'>London United Kingdom</h2>
+          <h2 className='font-custom mt-3 text-sm'>&quot;Front-end developer and avid reader.&quot;</h2>
         </div>
-        <div class="flex flex-col justify-center items-center space-y-2  mt-6">
+        <div class='flex flex-col justify-center items-center space-y-2  mt-6'>
           <Button>GitHub</Button>
           <Button>Frontend Mentor</Button>
           <Button>LinkedIn</Button>
