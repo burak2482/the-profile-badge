2a550166eae211dc03d0cc1726ca037567ad16c0
Burak Durmuşoğlu <108025275+burak2482@users.noreply.github.com>
2024-10-07 21:28:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/the-profile-badge',  // Projenizin GitHub repo adıyla değiştirmelisiniz
+  assetPrefix: '/the-profile-badge/',  // Aynı şekilde repo adınızı yazmalısınız
 };
 
 module.exports = nextConfig;
